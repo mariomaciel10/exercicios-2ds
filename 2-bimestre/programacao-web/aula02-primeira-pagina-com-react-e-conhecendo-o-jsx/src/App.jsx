fae6653d18fbb0950c9805c388220263dfb31244
Mario Pietro <piedromario7@gmail.com>
2026-04-29 01:14:27 +0000

    quase

--- 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -1,8 +1,14 @@
-export default function (){
-  return (
+export default function App() {
+  const favoritaMateria = "Programação Web"
+  const filmeFavorito = "Interestelar"
+
+  return(
     <div>
-      <h1></h1>
-      <p></p>
+      <h1>Oi meu nome é Eryck Fernando</h1>
+
+      <p>Minha matéria favorita é: {favoritaMateria}</p>
+
+      <p>Meu filme favorito é: {filmeFavorito}</p>
     </div>
   )
 }
