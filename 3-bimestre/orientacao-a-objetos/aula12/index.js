d38d0e4e30555d07ac501e4f2008dd83abc9c9b0
Mario Pietro <piedromario7@gmail.com>
2026-09-23 11:56:01 +0000

    ate o 16 marioso

--- 3-bimestre/orientacao-a-objetos/aula12/index.js
+++ 3-bimestre/orientacao-a-objetos/aula12/index.js
@@ -1,17 +1,23 @@
 class Usuario {
-   static quantidade = 0 
+    static quantidade = 0
 
-   constructor (nome) {
-      this.nome = nome
+    constructor(nome) {
+        this.nome = nome
+        Usuario.quantidade++
+    }
 
-      Usuario.quantidade++
-   }
+    static quantidadeUsuarios() {
+        return Usuario.quantidade
+    }
 }
 
 const usuario1 = new Usuario("Ana")
 const usuario2 = new Usuario("Carlos")
 const usuario3 = new Usuario("Maria")
 
-console.log(usuario1.quantidade)
-console.log(usuario2.quantidade)
-console.log(usuario3.quantidade)
+module.exports = {
+    Usuario,
+    usuario1,
+    usuario2,
+    usuario3
+}
